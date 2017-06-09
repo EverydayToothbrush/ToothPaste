8cde4a02843153bd8119db24144fe655a917fb0e
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-09 16:22:32 -0400

    added kick

---
+++ commands/kick.js
@@ -0,0 +1,25 @@
+const config = require('./config.json');
+const fs = require("fs");
+const cmds = require('./cmds.json');
+module.exports.run = async (client, message, args) => {
+  if(message.member.hasPermission("KICK_MEMBERS")) {
+    if (message.content.startsWith(config.prefix + "kick")) {
+      if(message.content.slice(6)) {
+        message.mentions.users.map(user => {
+          message.guild.member(user).kick()
+        });
+      } else {
+        message.channel.send('Who am I kicking?');
+      }
+    }
+
+
+  }
+  else{
+    message.channel.send("You Have Not Brushed Your Teeth!");
+  }
+}
+
+module.exports.help = {
+  name: 'kick'
+}
