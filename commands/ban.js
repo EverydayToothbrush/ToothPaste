69e53f36feee7964ae238bcb077f81ffa757db35
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-09 16:37:57 -0400

    added ban

---
+++ commands/ban.js
@@ -0,0 +1,25 @@
+const config = require('./config.json');
+const fs = require("fs");
+const cmds = require('./cmds.json');
+module.exports.run = async (client, message, args) => {
+  if(message.member.hasPermission("BAN_MEMBERS")) {
+    if (message.content.startsWith(config.prefix + "ban")) {
+      if(message.content.slice(5)) {
+        message.mentions.users.map(user => {
+          message.guild.member(user).ban()
+        });
+      } else {
+        message.channel.send('Who am I banning?');
+      }
+    }
+
+
+  }
+  else{
+    message.channel.send("You Have Not Brushed Your Teeth!");
+  }
+}
+
+module.exports.help = {
+  name: 'ban'
+}
