05f3fc02a49d50667d117365a10424d070438ce1
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-10 18:03:57 -0400

    caught error

--- commands/prefix.js
+++ commands/prefix.js
@@ -2,6 +2,9 @@ const config = require('./config.json');
 const fs = require("fs")
 module.exports.run = async (client, message, args) => {
   if (message.content.startsWith(config.prefix + "prefix")) {
+    if(!message.content.slice(8)) {
+      return;
+    }
     config.prefix = args[0];
     fs.writeFile('./config.json', JSON.stringify(config), (err) => {if(err) console.error(err)});
   }
