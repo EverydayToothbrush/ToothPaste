f09dac3f6803291a056c0c8f764b5794eb8b75d0
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-10 13:02:59 -0400

    added ability to mention

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,12 +2,23 @@ const config = require('./config.json');
 const fs = require("fs");
 module.exports.run = async (client, message, args) => {
   if (message.content.startsWith(config.prefix + "avatar")) {
-    message.channel.send({files: [
-      {
-        attachment: message.author.displayAvatarURL,
-        name: 'avatar.png'
-      }
-    ]});
+    if(message.content.slice(8)){
+      message.mentions.users.map(user => {
+        message.channel.send({files: [
+          {
+            attachment: user.displayAvatarURL,
+            name: 'avatar.png'
+          }
+        ]});
+      });
+    } else {
+      message.channel.send({files: [
+        {
+          attachment: message.author.displayAvatarURL,
+          name: 'avatar.png'
+        }
+      ]});
+    }
   }
 }
 
