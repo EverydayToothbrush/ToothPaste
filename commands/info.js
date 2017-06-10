5114e3997b528f719bba97f5587f106b8b1c1d57
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-10 18:55:37 -0400

    fixed lines

--- commands/info.js
+++ commands/info.js
@@ -7,24 +7,24 @@ module.exports.run = async (client, message, args) => {
       message.channel.send({embed:{
         color: 14365765,
         author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
+          name: message.member.displayName,
+          icon_url: message.author.displayAvatarURL
         },
         title: 'Info and Rules',
         description: 'Basic guidelines to follow, Mods have final say in any penal action which may be taken following sufficient warnings.',
         fields: [{
           name: 'Info',
-          value: 'Bot prefixes are \'**-**\' \'**$**\' \'**`**\' \'**!**\'.    Ranks are gained by chatting (see rules).      Check with !rank.     At level 5 you may ask for a specific color in the #general pinned messages (Dark Blue and Light Pink not listed but available).          If you play osu! we have a role and channel.          Post your anime list of any kind in #mal.'
+          value: 'Bot prefixes are \'**-**\' \'**$**\' \'**`**\' \'**!**\'.\nRanks are gained by chatting (see rules).\nCheck with !rank.\nAt level 5 you may ask for a specific color in the #general pinned messages (Dark Blue and Light Pink not listed but available).\nIf you play osu! we have a role and channel.\nPost your anime list of any kind in #mal.'
           },
           {
           name: 'Rules (including but not limited to)',
-          value: '**Don\'t be an ass** (e.g. trolling, harassing, racism, homophobia, etc. you get the idea), **Do not spam** (you may do so in #shitpost but it cant be the same thing repeating), **No NSFW anywhere** (including #shitpost).             **Keep discussions/content to approriate channels.**'
+          value: '**Don\'t be an ass** (e.g. trolling, harassing, racism, homophobia, etc. you get the idea)\n**Do not spam** (you may do so in #shitpost but it cant be the same thing repeating)\n**No NSFW anywhere** (including #shitpost).\n**Keep discussions/content to approriate channels.**'
           },
         ],
         timestamp: new Date(),
         footer: {
         icon_url: client.user.avatarURL,
-        text: 'dont forget to brush your teeth'
+        text: 'Dont Forget to Brush Your Teeth!'
         }
       }
       });
