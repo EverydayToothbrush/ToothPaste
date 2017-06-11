49b0b15fdd534734bb13288295c2ae997ef3fc91
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-11 19:21:02 -0400

    more finalized

--- commands/osuprofile.js
+++ commands/osuprofile.js
@@ -1,6 +1,8 @@
 const config = require('./config.json');
 const fs = require("fs");
 const osu = require('node-osu');
+const Discord = require('discord.js');
+
 
 var osuApi = new osu.Api(config.osukey, {
   notFoundAsError: true,
@@ -9,17 +11,30 @@ var osuApi = new osu.Api(config.osukey, {
 
 module.exports.run = async (client, message, args) => {
   if (message.content.startsWith(config.prefix + "osuprofile")) {
-    let osuuser = message.content.split(" ").slice(1);
-    osuApi.getUser({u: osuuser}).then(user => {
-      message.channel.send({embed: {
-        color: 16751103,
-        description: `${user.name}: ${user.pp.raw}`
-      }});
-    });
+    let osuuser = message.content.slice(12);
+    if(!osuuser) {
+      message.channel.send('No User');
+    } else {
+      osuApi.getUser({u: osuuser}).then(user => {
+        const embed = new Discord.RichEmbed()
+          .setTitle(`${user.name}'s Profile`)
+          .setAuthor(`${user.name}`, `https://a.ppy.sh/${user.id}`)
+          .setColor([255,137,233])
+          .setDescription(`**Rank**: ${user.pp.rank} (${user.pp.countryRank})${user.country} - ${user.pp.raw}`)
+          .setFooter(`Don't Forget to Brush Your Teeth!`, `${message.author.displayAvatarURL}`)
+          .setThumbnail(`https://a.ppy.sh/${user.id}`)
+          .setTimestamp()
+          .setURL(`https://osu.ppy.sh/u/${user.id}`)
+          .addField('Stats', `*Playcount*: ${user.counts.plays}\n\n**SS**: ${user.counts.SS}  **S**: ${user.counts.S}  **A**: ${user.counts.A}\n\n*Accuracy*: ${user.accuracyFormatted} *Level*: ${user.level}\n\n*Total Score*: ${user.scores.total}\n*Ranked Score*: ${user.scores.ranked}`);
+
+        message.channel.send({embed});
 
 
+      });
     }
   }
+}
+
 
 
 module.exports.help = {
