5a5a7ea96b0080a3c8c19acefa03bacb5d7757c0
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-13 13:01:13 -0400

    added

---
+++ commands/choose.js
@@ -0,0 +1,22 @@
+const config = require('./config.json');
+const fs = require("fs");
+
+module.exports.run = async (client, message, args) => {
+
+  if(message.content.startsWith(config.prefix + "choose")) {
+    let array = message.content.split(" ");
+    let options = array.slice(1);
+    if(options.length !== 0) {
+      message.channel.send("I choose " + `**${options[Math.floor(Math.random() * options.length)]}**`);
+    } else if (options.length === 0) {
+      message.channel.send("There's nothing to choose :thinking:");
+    }
+
+  }
+
+
+}
+
+module.exports.help = {
+  name: 'choose'
+}
