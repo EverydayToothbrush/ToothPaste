468b045628578024c598e44366efcceb88d4f055
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-13 16:30:48 -0400

    can now call specific command explanations

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,14 @@ const fs = require("fs");
 const cmds = require('./cmds.json');
 module.exports.run = async (client, message, args) => {
   if (message.content.startsWith(config.prefix + "help")) {
-    message.channel.send("Commands: " + cmds.cmd, {code: 'js'});
+    let total = message.content.split(" ");
+    let command = total.slice(1);
+    if(command.length === 0) {
+      message.channel.send("Commands: " + cmds.cmd, {code: 'js'});
+    } else if(command.length !== 0) {
+      message.channel.send(`${cmds[command]}`, {code: 'js'});
+    }
+
 
   }
 }
