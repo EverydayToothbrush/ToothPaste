e7f41e0419d0fe1d881929d7be14de8789567a8f
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-15 13:47:50 -0400

    updated permission

--- commands/kill.js
+++ commands/kill.js
@@ -2,15 +2,14 @@ const config = require('./config.json');
 const fs = require("fs");
 const cmds = require('./cmds.json');
 module.exports.run = async (client, message, args) => {
-  if(message.member.hasPermission("MANAGE_GUILD")) {
+  if(message.author.id === config.ownerID) {
     if (message.content.startsWith(config.prefix + "kill")) {
       await message.channel.send("Off To The Dentist o/");
       process.exit();
 
     }
-  }
-  else{
-    message.channel.send("You Have Not Brushed Your Teeth!");
+  } else {
+    message.channel.send("Please do not do that...");
   }
 }
 
