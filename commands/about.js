70679b8a49cc89c0957050b9b2f5b282b0f4be05
EverydayToothbrush <awesomedude162@gmail.com>
2017-06-27 21:33:56 -0400

    detail change

--- commands/about.js
+++ commands/about.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
       .setTitle('Lemme tell ya about myself!')
       .setAuthor(`${client.user.username}`, `${client.user.displayAvatarURL}`)
       .setColor([255,255,255])
-      .setDescription(`Created: ${client.user.createdAt}\nHello! I'm the companion to every toothbrush. I was created by <@181148305927962625> in JavaScript using the discord.js module.\nMy purpose was experimental and commands are added at the whims of my creator.\nI feature Join and Leave messages, and an assortment of other miscellaneous commands. You can view a list via '[help.'\nI also have SpoilerBot functionality (TimboKZ) and an (as of now) spammy cleverbot functionality.\nI would advise against pming the bot. \nAlways beware of your dental health! :smile: :wave:`)
+      .setDescription(`Created: ${client.user.createdAt}\nHello! I'm the companion to every toothbrush. I was created by <@181148305927962625> in JavaScript(Node.js) using the discord.js module.\nMy purpose was experimental and commands are added at the whims of my creator.\nI feature Join and Leave messages, and an assortment of other miscellaneous commands. You can view a list via '[help.'\nI also have SpoilerBot functionality (TimboKZ) and an (as of now) spammy cleverbot functionality.\nI would advise against pming the bot. \nAlways beware of your dental health! :smile: :wave:`)
       .setFooter('Don\'t Forget to Brush Your Teeth!', `${message.author.displayAvatarURL}`)
       .setThumbnail(`${client.user.displayAvatarURL}`)
       .setTimestamp()
