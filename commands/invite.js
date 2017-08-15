82d337341ff1495f7cdd1d911478423d60da6576
EverydayToothbrush <awesomedude162@gmail.com>
2017-08-15 15:52:41 -0400

    Added invite to help

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 module.exports.run = async (client, message, args) => {
   if(message.content.startsWith(process.env.PREFIX + "invite")) {
-    client.generateInvite(['SEND_MESSAGES', 'MANAGE_GUILD'])
+    client.generateInvite(['ADMINISTRATOR', 'SEND_MESSAGES', 'MANAGE_GUILD'])
       .then(link => {
         message.channel.send(`Bot invite link: ${link}`);
       });
