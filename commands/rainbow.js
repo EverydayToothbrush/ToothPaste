58fa1a74e05cba5ff76ebce53a50f58f487a5452
EverydayToothbrush <awesomedude162@gmail.com>
2017-11-02 20:03:48 -0400

    ugh2

--- commands/rainbow.js
+++ commands/rainbow.js
@@ -1,16 +1,16 @@
 const fs = require("fs");
 const cmds = require('./cmds.json');
-const timeout;
 module.exports.run = async (client, message, args) => {
   if(message.author.id === process.env.OWNER_ID) {
     if (message.content.startsWith(process.env.PREFIX + "rainbow")) {
       let arg = message.content.slice(9);
+      var timeout;
       if(arg != 'off') {
         let rolename = message.member.guild.roles.find('name', arg);
         if(rolename) {
             function change() {
                 rolename.setColor('RANDOM');
-                timeout = setTimeout(function color(){rolename.setColor('RANDOM')}, 60000);
+                timeout = setTimeout(function color(){rolename.setColor('RANDOM'); change();}, 60000);
             }
             change();
         } else {
