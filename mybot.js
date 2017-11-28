9ca4bdd946a63dc1684280687673e21428e80dd1
EverydayToothbrush <awesomedude162@gmail.com>
2017-11-28 13:00:50 -0500

    toString, got rid of outer if

--- mybot.js
+++ mybot.js
@@ -99,15 +99,14 @@ client.on("message", (message) => {
     });
   }
 
-  if(message.content.startsWith(`${reply}`)) {
-    let array = message.content.split(' ');
-    let source = array[0];
-    if(message.content.startsWith('ayy')) {
-      message.channel.send(`${reply.ayy + 'o'.repeat(source.length - 3)}`);
-    } else {
-      message.channel.send(`${reply[message.content]}`);
-    }
-  }
+
+  let array = message.content.split(' ');
+  let source = array[0];
+  if(message.content.startsWith('ayy')) {
+    message.channel.send(`${reply.ayy + 'o'.repeat(source.length - 3)}`);
+  } else {
+    message.channel.send(`${reply[message.content]}`);
+  }  
 
   if(message.channel.type === 'dm' || message.mentions.users.find('username', 'Toothpaste')) {
     if(message.content.startsWith(process.env.PREFIX)) return;
@@ -134,9 +133,9 @@ client.on("message", (message) => {
   if(!message.content.startsWith(process.env.PREFIX)) return;
   if(message.author.bot) return;
 
-  let messageArray = message.content.split(/\s+/g)
-  let command = messageArray[0].toLowerCase()
-  let args = messageArray.slice(1).toLowerCase();
+  let messageArray = message.content.split(/\s+/g).toString().toLowerCase()
+  let command = messageArray[0].toString().toLowerCase()
+  let args = messageArray.slice(1);
 
   let commands = client.commands.get(command.slice(process.env.PREFIX.length));
   if(commands) commands.run(client, message, args);
