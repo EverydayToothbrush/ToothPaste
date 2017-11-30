5306651f1f7e9e56fe10b193725d2046c6260a01
EverydayToothbrush <awesomedude162@gmail.com>
2017-11-30 01:29:57 -0500

    regex + console log

--- commands/emoji.js
+++ commands/emoji.js
@@ -5,14 +5,15 @@ module.exports.run = async (client, message, args) => {
   if(message.content.startsWith(process.env.PREFIX + "emoji")) {
     let arg = message.content.split(' ');
     let emoji = arg[1];
-    if(message.guild.emojis.find('id', `${emoji}`)) {
+    if(message.guild.emojis.find('id', `${emoji.replace(/:\w+:/g,'')}`)) {
       message.channel.send({files: [
         {
-          attachment: message.guild.emojis.find('name', `${emoji}`).url,
-          name: `${message.guild.emojis.find('name', `${emoji}`).url.split('/').pop()}`
+          attachment: message.guild.emojis.find('id', `${emoji.replace(/:\w+:/g,'')}`).url,
+          name: `${message.guild.emojis.find('id', `${emoji.replace(/:\w+:/g,'')}`).url.split('/').pop()}`
         }
       ]});
     } else {
+      console.log(message.content);
       message.channel.send("Which one are you looking for?");
     }
 
