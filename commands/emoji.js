2ecba880a997394e9bb45741e94cfeda537014bf
EverydayToothbrush <awesomedude162@gmail.com>
2017-11-30 01:52:54 -0500

    change to name

--- commands/emoji.js
+++ commands/emoji.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 
 module.exports.run = async (client, message, args) => {
   if(message.content.startsWith(process.env.PREFIX + "emoji")) {
-    if(message.guild.emojis.find('id', `${message.content.split(' ').pop().replace(/:\w+:/g,'')}`)) {
+    if(message.guild.emojis.find('name', `${message.content.split(' ').pop().replace(/[<>:\d]/g,'')}`)) {
       message.channel.send({files: [
         {
-          attachment: message.guild.emojis.find('id', `${message.content.split(' ').pop().replace(/:\w+:/g,'')}`).url,
-          name: `${message.guild.emojis.find('id', `${message.content.split(' ').pop().replace(/:\w+:/g,'')}`).url.split('/').pop()}`
+          attachment: message.guild.emojis.find('name', `${message.content.split(' ').pop().replace(/[<>:\d]/g,'')}`).url,
+          name: `${message.guild.emojis.find('name', `${message.content.split(' ').pop().replace(/[<>:\d]/g,'')}`).url.split('/').pop()}`
         }
       ]});
     } else {
