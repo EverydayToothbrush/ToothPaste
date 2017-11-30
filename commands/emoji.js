be1f0d45f765d9e9ce3145bae63777e7ee02f561
EverydayToothbrush <awesomedude162@gmail.com>
2017-11-30 11:03:33 -0500

    trying to support global

--- commands/emoji.js
+++ commands/emoji.js
@@ -3,7 +3,15 @@ const fs = require("fs");
 
 module.exports.run = async (client, message, args) => {
   if(message.content.startsWith(process.env.PREFIX + "emoji")) {
-    if(message.guild.emojis.find('name', `${message.content.split(' ').pop().replace(/[<>:\d]/g,'')}`)) {
+    let emote = message.content.split(" ");
+    if(emote[1]) {
+      message.channel.send({files: [
+        {
+          attachment: `https://cdn.discordapp.com/emojis/${message.content.split(' ').pop().replace(/\D+/g,'')}.png`,
+          name: `${message.content.split(' ').pop().replace(/\D+/g,'')}.png`
+        }
+      ]});
+    } else if(message.guild.emojis.find('name', `${message.content.split(' ').pop().replace(/[<>:\d]/g,'')}`)) {
       message.channel.send({files: [
         {
           attachment: message.guild.emojis.find('name', `${message.content.split(' ').pop().replace(/[<>:\d]/g,'')}`).url,
@@ -11,7 +19,7 @@ module.exports.run = async (client, message, args) => {
         }
       ]});
     } else {
-      message.channel.send("Invalid emoji, please choose a custom emoji from this server.");
+      message.channel.send("Invalid emoji, please choose a custom emoji.");
     }
 
   }
