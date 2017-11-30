dfafb8a037fa113e4d470c7e573f5c42417e1199
EverydayToothbrush <awesomedude162@gmail.com>
2017-11-30 01:40:30 -0500

    remove another variable

--- commands/emoji.js
+++ commands/emoji.js
@@ -3,12 +3,11 @@ const fs = require("fs");
 
 module.exports.run = async (client, message, args) => {
   if(message.content.startsWith(process.env.PREFIX + "emoji")) {
-    let arg = message.content.split(' ');
-    if(message.guild.emojis.find('id', `${arg[1].replace(/:\w+:/g,'')}`)) {
+    if(message.guild.emojis.find('id', `${message.content.split(' ').pop().replace(/:\w+:/g,'')}`)) {
       message.channel.send({files: [
         {
-          attachment: message.guild.emojis.find('id', `${arg[1].replace(/:\w+:/g,'')}`).url,
-          name: `${message.guild.emojis.find('id', `${arg[1].replace(/:\w+:/g,'')}`).url.split('/').pop()}`
+          attachment: message.guild.emojis.find('id', `${message.content.split(' ').pop().replace(/:\w+:/g,'')}`).url,
+          name: `${message.guild.emojis.find('id', `${message.content.split(' ').pop().replace(/:\w+:/g,'')}`).url.split('/').pop()}`
         }
       ]});
     } else {
