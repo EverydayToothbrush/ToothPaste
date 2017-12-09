ab0872f2453d9f22baf36a13a008c00750f275bb
EverydayToothbrush <awesomedude162@gmail.com>
2017-12-08 19:16:13 -0500

    prune for user?

--- commands/prune.js
+++ commands/prune.js
@@ -3,10 +3,23 @@ const fs = require("fs");
 
 module.exports.run = async (client, message, args) => {
   if (message.member.hasPermission("MANAGE_MESSAGES")) {
-    let messagecount = parseInt(message.content.split(" ").pop());
-    if(!Number.isInteger(messagecount)) {
-      message.reply("That is not a number");
+    let user = message.mentions.users.first()
+    let msgArray = message.content.split(" ");
+    let messagecount = parseInt(msgArray[1]);
+    if(!Number.isInteger(messagecount) && !user) {
+      message.reply("There is no amount or user specified");
       return;
+    } else if(!Number.isInteger(messagecount) && user) {
+      message.reply("There is no amount specified");
+      return;
+    } else if(Number.isInteger(messagecount) && user) {
+      message.channel.fetchMessages({limit: messagecount})
+        .then(messages => {
+          let mentionmsg = messages.filter(m => m.author.id === user).array().slice(0, messagecount);
+          message.channel.bulkDelete(mentionmsg);
+        });
+        message.channel.send("Deleted " + messagecount + " messages", {code: 'js'})
+          .then(message => message.delete(2000));
     } else {
     message.channel.fetchMessages({limit: messagecount})
       .then(messages => message.channel.bulkDelete(messages));
