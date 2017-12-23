2bb01d781f1245c1e788906cf9023e5fb3632afd
EverydayToothbrush <awesomedude162@gmail.com>
2017-12-22 19:02:33 -0500

    missing BRACES AGAIN

--- commands/reverse.js
+++ commands/reverse.js
@@ -4,11 +4,11 @@ const fs = require("fs");
 module.exports.run = async (client, message, args) => {
   if(!message.content.slice(9)) {
     message.channel.fetchMessages({limit: 2})
-      .then(msg =>
+      .then(msg => {
         let actualStr = msg.last();
         let reverseStr = actualStr.split("").reverse().join();
         message.channel.send(reverseStr);
-      );
+      });
   } else {
     let str = message.content.slice(9);
     let reversed = str.split("").reverse().join();
