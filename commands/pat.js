a99cc02ac709741c05ec945763d2b83fcc076a9e
EverydayToothbrush <awesomedude162@gmail.com>
2018-03-03 16:03:46 -0500

    added pat

---
+++ commands/pat.js
@@ -0,0 +1,27 @@
+
+const fs = require("fs");
+module.exports.run = async (client, message, args) => {
+  let patimg = fs.readdirSync('./pats/');
+  if(message.content.slice(5)) {
+    let person = message.mentions.users.first();
+    message.channel.send({files: [
+      {
+        attachment: `./pats/${patimg[Math.floor(Math.random() * patimg.length)]}`,
+        name: 'headpat.gif',
+        content: `${message.author.username} gave <@${person.id}> a pat!`
+      }
+    ]});
+  } else {
+    message.channel.send({files: [
+      {
+        attachment: `./pats/${patimg[Math.floor(Math.random() * patimg.length)]}`,
+        name: 'headpat.gif'
+      }
+    ]});
+  }
+}
+
+
+module.exports.help = {
+  name: 'pat'
+}
