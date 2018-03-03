3a9e69f40a0ae8aec180aeef896f54f75ca674a7
EverydayToothbrush <awesomedude162@gmail.com>
2018-03-03 16:23:24 -0500

    content to image

--- commands/pat.js
+++ commands/pat.js
@@ -4,13 +4,13 @@ module.exports.run = async (client, message, args) => {
   let patimg = fs.readdirSync('./commands/pats/');
   if(message.content.slice(5)) {
     let person = message.mentions.users.first();
-    message.channel.send({files: [
-      {
-        attachment: `./commands/pats/${patimg[Math.floor(Math.random() * patimg.length)]}`,
-        name: 'headpat.gif',
-        content: `${message.author.username} gave <@${person.id}> a pat!`
-      }
-    ]});
+    message.channel.send(`${message.author.username} gave <@${person.id}> a pat!`,
+      {files: [
+        {
+          attachment: `./commands/pats/${patimg[Math.floor(Math.random() * patimg.length)]}`,
+          name: 'headpat.gif'
+        }
+      ]});
   } else {
     message.channel.send({files: [
       {
