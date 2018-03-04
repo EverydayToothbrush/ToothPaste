1d50656df712be1ca244abfc93a2eb3bdc4fe634
EverydayToothbrush <awesomedude162@gmail.com>
2018-03-04 17:06:18 -0500

    trying to handle nicknames

--- commands/pat.js
+++ commands/pat.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports.run = async (client, message, args) => {
   let patimg = fs.readdirSync('./commands/pats/');
   let msgarry = message.content.split(" ");
-  if(msgarry[1] && msgarry[1] == message.mentions.users.first()) {
+  if(msgarry[1] && (msgarry[1].replace(/<@!?|>/g, '') == message.mentions.users.first().id)) {
     let img = patimg[Math.floor(Math.random() * patimg.length)];
     let person = message.mentions.users.first();
     message.channel.send(`${message.author.username} gave <@${person.id}> a pat!`,
