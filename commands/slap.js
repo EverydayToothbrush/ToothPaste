7a73bf6e3fa799c8ff89581afcb3425ad7da25b9
EverydayToothbrush <awesomedude162@gmail.com>
2018-03-05 12:56:58 -0500

    slap command

---
+++ commands/slap.js
@@ -0,0 +1,33 @@
+
+const fs = require("fs");
+module.exports.run = async (client, message, args) => {
+  let slapimg = fs.readdirSync('./commands/slaps/');
+  let msgarry = message.content.split(" ");
+  if(slapimg.length == 0) {
+    message.channel.send("There are no images.");
+  }
+  if(msgarry[1] && (message.mentions.users.first() != undefined)) {
+    let img = slapimg[Math.floor(Math.random() * slapimg.length)];
+    let person = message.mentions.users.first();
+    message.channel.send(`${message.author.username} slapped <@${person.id}> !`,
+      {files: [
+        {
+          attachment: `./commands/slaps/${img}`,
+          name: `${img}`
+        }
+      ]});
+  } else {
+    let img = slapimg[Math.floor(Math.random() * slapimg.length)];
+    message.channel.send({files: [
+      {
+        attachment: `./commands/slaps/${img}`,
+        name: `${img}`
+      }
+    ]});
+  }
+}
+
+
+module.exports.help = {
+  name: 'slap'
+}
